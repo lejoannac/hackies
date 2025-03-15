a7a550307f54d205dbbb5b71ee721e9457d19653
lejoannac <jxcai534@gmail.com>
2025-03-15 16:41:39 -0700

    update

--- popup2.js
+++ popup2.js
@@ -1,4 +1,6 @@
-var button5 = document.getElementById("btn5");
-button5.addEventListener("click", function() {
-    chrome.tabs.create({ url: "https://www.youtube.com/watch?v=DSG53BsUYd0" });
+document.addEventListener("DOMContentLoaded", function() {
+    var btn5 = document.getElementById("btn5");
+    btn5.addEventListener("click", function() {
+        chrome.tabs.create({ url: "https://www.youtube.com/watch?v=DSG53BsUYd0" });
+    });
 });
